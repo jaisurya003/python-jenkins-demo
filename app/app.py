57000cbb828506a6897c692d3c27a2774164365e
jaisurya003 <146946666+jaisurya003@users.noreply.github.com>
2026-06-04 18:00:39 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello from DevOp THIS IS JAISURYAFFFFF Project 🚀"})
+    return jsonify({"message": "Hello from DevOp THIS IS JAISURYA Project 🚀"})
 
 @app.route('/health')
 def health():
